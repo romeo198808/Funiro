ad1ae2fd58f9bc02e7bc32c3d844ed1f0f56dbf0
romeo198808 <romeo1988.08.03@gmail.com>
2023-04-10 21:20:31 +0300

    start

---
+++ gulpfile.js
@@ -0,0 +1,102 @@
+"use strict"
+
+let gulp = require('gulp');
+let scss = require('gulp-scss');
+let plumber =require('gulp-plumber');
+let cssmin = require('gulp-cssmin');
+let rename = require('gulp-rename');
+let autoprefixer = require('gulp-autoprefixer');
+let svgo = require('gulp-svgo');
+let imagemin = require('gulp-imagemin');
+let webp = require('gulp-webp');
+let svgstore = require('gulp-svgstore');
+let clean  = require('gulp-clean');
+let copy = require('gulp-copy');
+const { use } = require('browser-sync');
+let server = require('browser-sync').create();
+
+
+
+
+gulp.task('style', function(){
+	return gulp.src('src/scss/style.scss')
+	.pipe(plumber())
+	.pipe(scss())
+	.pipe(autoprefixer())
+	.pipe(gulp.dest('src/css'))
+	.pipe(cssmin())
+	.pipe(rename('style.min.css'))
+	.pipe(gulp.dest('src/css'))
+	.pipe(server.stream())
+});
+
+gulp.task('svg-sprite', ()=> {
+	return gulp.src('src/img/icon-*.svg')
+	.pipe(svgstore({inlineSvg: true}))
+	.pipe(rename('sprite.svg'))
+	.pipe(gulp.dest('src/img'))
+});
+
+gulp.task('webp', ()=> {
+	return gulp.src('src/img/*.{jpg,png}')
+	.pipe(webp())
+	.pipe(gulp.dest('src/img'))
+});
+
+gulp.task('image', ()=> {
+	return gulp.src('src/img/*.{png,jpg,svg}')
+	.pipe(imagemin([
+		imagemin.mozjpeg({quality: 70, progressive: true}),
+		imagemin.optipng({optimizationLevel:3}),
+		imagemin.svgo()
+	]))
+	.pipe(gulp.dest('src/img'))
+});
+
+gulp.task('svgo', ()=> {
+	return gulp.src('src/img/*.svg')
+	.pipe(svgo())
+	.pipe(gulp.dest('src/img'))
+	.pipe(server.stream())
+});
+
+gulp.task('normalize', ()=> {
+	return gulp.src('src/css/normalize.css')
+	.pipe(cssmin())
+	.pipe(rename('normalize.min.css'))
+	.pipe(gulp.dest('src/css'))
+	.pipe(server.stream())
+});
+
+gulp.task('clean', ()=> {
+	return gulp.src('prod')
+	.pipe(clean());
+});
+
+gulp.task('copy', ()=> {
+	return gulp.src([
+		'src/*.html',
+		'src/css/**/*.css',
+		'src/fonts/**/*.*',
+		'src/img/**/*.*',
+		'src/js/**/*.js',
+	], {base:'src'})
+	// .pipe(copy('prod'));
+	.pipe(gulp.dest('prod/'));
+});
+
+gulp.task('serve', ()=> {
+	server.init({
+		server: "src",
+		notify: false,
+		open: true,
+		cors: true,
+		ui: false
+	});
+	gulp.watch('src/scss/**/*.scss', gulp.series('style')).on('change', server.reload);
+	gulp.watch('src/*.html').on('change', server.reload);
+});
+
+gulp.task('start', gulp.series('image', 'webp', 'svg-sprite', 'style', 'serve'));
+
+gulp.task('prod', gulp.series('clean', 'image', 'webp', 'svg-sprite', 'style', 'copy'));
